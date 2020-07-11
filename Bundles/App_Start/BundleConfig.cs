1c9b9df661e25c22ac683e7856b8aaf8f50e8f9e
Zaibi <zaibi@gmail.com>
2020-07-11 13:24:54 +0500

    Bundles in MVC 5

---
+++ Bundles/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Bundles
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            var bundle = new ScriptBundle("~/bundles/js");
+
+            bundle.Include("~/Content/JS/JavaScript1.js",
+                "~/Content/JS/JavaScript2.js",
+                "~/Content/JS/JavaScript3.js",
+                "~/Content/JS/JavaScript4.js",
+                "~/Content/JS/JavaScript5.js"
+                );
+            bundles.Add(bundle);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
